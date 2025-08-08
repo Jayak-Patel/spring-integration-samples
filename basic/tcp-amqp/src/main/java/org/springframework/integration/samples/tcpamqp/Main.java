d4078a688e484d55af91d084b831dde4f20c88e4
Jayak Patel <jayakpatel1@gmail.com>
2025-08-08 17:07:43 -0400

    Fix batch of 1 issues

--- basic/tcp-amqp/src/main/java/org/springframework/integration/samples/tcpamqp/Main.java
+++ basic/tcp-amqp/src/main/java/org/springframework/integration/samples/tcpamqp/Main.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *	  https://www.apache.org/licenses/LICENSE-2.0
+ *      https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -29,44 +29,44 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public final class Main {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
-	private Main() { }
+    private Main() { }
 
-	/**
-	 * Load the Spring Integration Application Context
-	 *
-	 * @param args - command line arguments
-	 */
-	public static void main(final String... args) throws Exception {
+    /**
+     * Load the Spring Integration Application Context
+     *
+     * @param args - command line arguments
+     */
+    public static void main(final String... args) throws Exception {
 
-		LOGGER.info("\n========================================================="
-				  + "\n														 "
-				  + "\n		  Welcome to Spring Integration!				 "
-				  + "\n														 "
-				  + "\n	For more information please visit:				   "
-				  + "\n	https://www.springsource.org/spring-integration	   "
-				  + "\n														 "
-				  + "\n=========================================================" );
+        LOGGER.info("\n=========================================================\n"
+                  + "                                                        \n"
+                  + "          Welcome to Spring Integration!                \n"
+                  + "                                                        \n"
+                  + "    For more information please visit:                  \n"
+                  + "    https://www.springsource.org/spring-integration     \n"
+                  + "                                                        \n"
+                  + "=========================================================");
 
-		final AbstractApplicationContext context =
-				new ClassPathXmlApplicationContext("classpath:META-INF/spring/integration/*-context.xml");
+        final AbstractApplicationContext context =
+                new ClassPathXmlApplicationContext("classpath:META-INF/spring/integration/*-context.xml");
 
-		LOGGER.info("\n========================================================="
-				  + "\n														  "
-				  + "\n	This is the TCP-AMQP Sample -						 "
-				  + "\n														  "
-				  + "\n	Start a netcat, listening on port 11112 -			 "
-				  + "\n	netcat -l 11112									   "
-				  + "\n														  "
-				  + "\n	In another terminal, telnet to localhost 11111		"
-				  + "\n	Enter text and you will see it echoed to the netcat   "
-				  + "\n														  "
-				  + "\n	Press Enter in this console to terminate			  "
-				  + "\n														  "
-				  + "\n=========================================================" );
+        LOGGER.info("\n=========================================================\n"
+                  + "                                                         \n"
+                  + "    This is the TCP-AMQP Sample -                         \n"
+                  + "                                                         \n"
+                  + "    Start a netcat, listening on port 11112 -            \n"
+                  + "    netcat -l 11112                                       \n"
+                  + "                                                         \n"
+                  + "    In another terminal, telnet to localhost 11111       \n"
+                  + "    Enter text and you will see it echoed to the netcat  \n"
+                  + "                                                         \n"
+                  + "    Press Enter in this console to terminate             \n"
+                  + "                                                         \n"
+                  + "=========================================================");
 
-		System.in.read();
-		context.close();
-	}
+        System.in.read();
+        context.close();
+    }
 }
