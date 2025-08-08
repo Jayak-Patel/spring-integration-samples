e063084d3d93264f900d40a9f62e1d3432f6067d
Jayak Patel <jayakpatel1@gmail.com>
2025-08-08 14:42:32 -0400

    Fix batch of 1 issues

--- basic/ftp/src/test/java/org/springframework/integration/samples/ftp/BaseFtpTest.java
+++ basic/ftp/src/test/java/org/springframework/integration/samples/ftp/BaseFtpTest.java
@@ -51,7 +51,7 @@ public abstract class BaseFtpTest {
 	public static final String SERVER_PORT_SYSTEM_PROPERTY = "availableServerPort";
 
 	@TempDir
-	public static File temporaryFolder;
+	public static final File temporaryFolder = new File(LOCAL_FTP_TEMP_DIR);
 
 	public static FtpServer server;
 
