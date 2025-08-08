985247043fe5840912e0d58ccc13cd4790e8a076
Jayak Patel <jayakpatel1@gmail.com>
2025-08-08 19:30:22 -0400

    Fix batch of 5 issues

--- intermediate/testcontainers-rabbitmq/src/test/java/org/springframework/integration/samples/testcontainersrabbitmq/Receiver.java
+++ intermediate/testcontainers-rabbitmq/src/test/java/org/springframework/integration/samples/testcontainersrabbitmq/Receiver.java
@@ -18,7 +18,7 @@ package org.springframework.integration.samples.testcontainersrabbitmq;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Random;
+import java.security.SecureRandom;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -39,7 +39,7 @@ public class Receiver {
 
     private static final Map<Integer, String> messages = new HashMap<>();
 
-    private final Random random = new Random();
+    private final SecureRandom random = new SecureRandom();
 
     static {
         messages.put(1, "This is message 1");
