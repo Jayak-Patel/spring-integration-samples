0c900fb8d3e102903e4cc42eb5b5e4cd53e34308
Jayak Patel <jayakpatel1@gmail.com>
2025-08-09 00:56:44 -0400

    Fix batch of 25 issues

--- advanced/dynamic-tcp-client/src/test/java/org/springframework/integration/samples/dynamictcp/DynamicTcpClientApplicationTests.java
+++ advanced/dynamic-tcp-client/src/test/java/org/springframework/integration/samples/dynamictcp/DynamicTcpClientApplicationTests.java
@@ -1,17 +1,15 @@
 package org.springframework.integration.samples.dynamictcp;
 
 import org.junit.jupiter.api.Test;
-
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class DynamicTcpClientApplicationTests {
 
-	@Test
-	public void contextLoads() {
-		// This method is empty because the test is only meant to load the Spring context
-		// and not perform any specific assertions. If you want to add assertions or
-		// other test logic, you can do so here.
-	}
-
+    @Test
+    public void contextLoads() {
+        // This method is empty because the test is only meant to load the Spring context
+        // and not perform any specific assertions. If you want to add assertions or
+        // other test logic, you can do so here.
+    }
 }
