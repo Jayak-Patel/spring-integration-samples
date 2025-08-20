dcecc9a8963bc3e8cfd64377b991a0013d030589
Jayak Patel <jayakpatel1@gmail.com>
2025-08-20 17:40:03 -0400

    Fix batch of 5 issues

--- basic/mongodb/src/main/java/org/springframework/integration/samples/mongodb/outbound/MongoDbOutboundAdapterDemo.java
+++ basic/mongodb/src/main/java/org/springframework/integration/samples/mongodb/outbound/MongoDbOutboundAdapterDemo.java
@@ -21,86 +21,72 @@ import org.springframework.integration.samples.mongodb.domain.Person;
 import org.springframework.integration.samples.mongodb.util.DemoUtils;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.support.GenericMessage;
+
 /**
-*
-* @author Oleg Zhurakousky
-* @author Gary Russell
-*/
+ * @author Oleg Zhurakousky
+ * @author Gary Russell
+ */
 public class MongoDbOutboundAdapterDemo {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) throws Exception {
-		DemoUtils.prepareMongoFactory(); // will clean up MongoDB
-		new MongoDbOutboundAdapterDemo().runDefaultAdapter();
-//		new MongoDbOutboundAdapterDemo().runAdapterWithConverter();
-	}
+    /**
+     * @param args
+     */
+    public static void main(String[] args) throws Exception {
+        DemoUtils.prepareMongoFactory(); // will clean up MongoDB
+        new MongoDbOutboundAdapterDemo().runDefaultAdapter();
+    }
 
-	public void runDefaultAdapter() throws Exception {
+    public void runDefaultAdapter() throws Exception {
+        try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("mongodb-out-config.xml", MongoDbOutboundAdapterDemo.class)) {
+            MessageChannel messageChannel = context.getBean("defaultAdapter", MessageChannel.class);
+            messageChannel.send(new GenericMessage<>(createPersonA()));
+            messageChannel.send(new GenericMessage<>(createPersonB()));
+            messageChannel.send(new GenericMessage<>(createPersonC()));
+        }
+    }
 
-		@SuppressWarnings("resource")
-		ClassPathXmlApplicationContext context =
-				new ClassPathXmlApplicationContext("mongodb-out-config.xml", MongoDbOutboundAdapterDemo.class);
+    private Person createPersonA() {
+        Address address = new Address();
+        address.setCity("Palo Alto");
+        address.setStreet("3401 Hillview Ave");
+        address.setZip("94304");
+        address.setState("CA");
 
-		MessageChannel messageChannel = context.getBean("defaultAdapter", MessageChannel.class);
-		messageChannel.send(new GenericMessage<Person>(this.createPersonA()));
-		messageChannel.send(new GenericMessage<Person>(this.createPersonB()));
-		messageChannel.send(new GenericMessage<Person>(this.createPersonC()));
-	}
+        Person person = new Person();
+        person.setFname("John");
+        person.setLname("Doe");
+        person.setAddress(address);
 
-	public void runAdapterWithConverter() throws Exception {
+        return person;
+    }
 
-		@SuppressWarnings("resource")
-		ClassPathXmlApplicationContext context =
-				new ClassPathXmlApplicationContext("mongodb-out-config.xml", MongoDbOutboundAdapterDemo.class);
+    private Person createPersonB() {
+        Address address = new Address();
+        address.setCity("San Francisco");
+        address.setStreet("123 Main st");
+        address.setZip("94115");
+        address.setState("CA");
 
-		MessageChannel messageChannel = context.getBean("adapterWithConverter", MessageChannel.class);
-		messageChannel.send(new GenericMessage<String>("John, Dow, Palo Alto, 3401 Hillview Ave, 94304, CA"));
-	}
+        Person person = new Person();
+        person.setFname("Josh");
+        person.setLname("Doe");
+        person.setAddress(address);
 
-	private Person createPersonA(){
-		Address address = new Address();
-		address.setCity("Palo Alto");
-		address.setStreet("3401 Hillview Ave");
-		address.setZip("94304");
-		address.setState("CA");
+        return person;
+    }
 
-		Person person = new Person();
-		person.setFname("John");
-		person.setLname("Doe");
-		person.setAddress(address);
+    private Person createPersonC() {
+        Address address = new Address();
+        address.setCity("Philadelphia");
+        address.setStreet("2323 Market st");
+        address.setZip("19152");
+        address.setState("PA");
 
-		return person;
-	}
+        Person person = new Person();
+        person.setFname("Jane");
+        person.setLname("Doe");
+        person.setAddress(address);
 
-	private Person createPersonB(){
-		Address address = new Address();
-		address.setCity("San Francisco");
-		address.setStreet("123 Main st");
-		address.setZip("94115");
-		address.setState("CA");
-
-		Person person = new Person();
-		person.setFname("Josh");
-		person.setLname("Doe");
-		person.setAddress(address);
-
-		return person;
-	}
-
-	private Person createPersonC(){
-		Address address = new Address();
-		address.setCity("Philadelphia");
-		address.setStreet("2323 Market st");
-		address.setZip("19152");
-		address.setState("PA");
-
-		Person person = new Person();
-		person.setFname("Jane");
-		person.setLname("Doe");
-		person.setAddress(address);
-
-		return person;
-	}
+        return person;
+    }
 }
